43674662cc38afa78782bb48c01f39939674a910
MaxSingh01 <ranjitsingh131101@gmail.com>
2022-10-09 10:43:04 +0530

    changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import Banner from "../components/banner/banner";
 import styles from "../styles/home.module.css";
+import feature from "../styles/feature.module.css";
 import logo from "../public/img.jpg";
 import Image from "next/image";
 import { DEALS } from "../data";
@@ -29,6 +30,25 @@ const Home: NextPage = () => {
           />
         </div>
         <div className={styles.right_box}>
+          <DealsTabs />
+        </div>
+      </div>
+      <div className={feature.section_feature}>
+        <div className={feature.left_box}>
+          <div className={feature.header}>
+            <span>Featured</span>
+            <h2 className={feature.section_title}>
+              Top categories <br />
+              this week
+            </h2>
+          </div>
+          <div>
+            <button className={feature.btn}>prev</button>
+            <button className={feature.btn}>next</button>
+          </div>
+          <p className={feature.footer_para}>Full catalog</p>
+        </div>
+        <div className={feature.right_box}>
           <DealsTabs />
         </div>
       </div>
