f2fd6ea7df2ba9734308fd08713cd6fdec89b649
Decode-Your-Design <109458963+Decode-Your-Design@users.noreply.github.com>
2022-12-13 22:48:52 +0530

    remove Image tag and added img in vendor product list

--- pages/vendor-product-list.tsx
+++ pages/vendor-product-list.tsx
@@ -17,13 +17,13 @@ export default function VendorProductList() {
   const [addProduct, setAddProduct] = useState(true);
   const [productsData, setProductsData] = useState([]);
   const [productData, setProductData] = useState<any>({
-    name:"",
-    price:"",
-    shortDesc:"",
-    longDesc:"",
-    vehicleType:""
+    name: "",
+    price: "",
+    shortDesc: "",
+    longDesc: "",
+    vehicleType: "",
   });
-  const [loading,setLoading] = useState(true)
+  const [loading, setLoading] = useState(true);
   // const accessToken = localStorage.getItem('accessToken')
 
   const getVendorProducts = async () => {
@@ -35,14 +35,13 @@ export default function VendorProductList() {
         },
       }
     );
-    if(response.data.success){
-    setProductsData(response.data.result);
-    setLoading(false)
-    }
-    else{
-      sessionStorage.setItem('backgroundColor',"red")
-      sessionStorage.setItem('toastifyContent',response.data.message)
-      setOpenToastify(true)
+    if (response.data.success) {
+      setProductsData(response.data.result);
+      setLoading(false);
+    } else {
+      sessionStorage.setItem("backgroundColor", "red");
+      sessionStorage.setItem("toastifyContent", response.data.message);
+      setOpenToastify(true);
     }
   };
 
@@ -50,16 +49,14 @@ export default function VendorProductList() {
     if (localStorage.getItem("userType") == "Vendor") {
       // getVendorProducts();
       // setProductData({})
-      getVendorProducts()
-    
+      getVendorProducts();
     } else {
       router.push("/");
     }
   }, [router]);
 
-
   const deleteProduct = async (productId: String) => {
-    setLoading(true)
+    setLoading(true);
     const response = await axios.post(
       `https://lobster-app-ymo47.ondigitalocean.app/api/product/removeProduct/${productId}`,
       productData,
@@ -71,17 +68,15 @@ export default function VendorProductList() {
     );
     // setOpenForm(false);
 
-    if(response.data.success){
-
+    if (response.data.success) {
       sessionStorage.setItem("backgroundColor", "red");
       sessionStorage.setItem("toastifyContent", response.data.message);
       setOpenToastify(true);
       getVendorProducts();
-    }
-    else{
-      sessionStorage.setItem('backgroundColor',"red")
-      sessionStorage.setItem("toastifyContent",response.data.message)
-      setOpenToastify(true)
+    } else {
+      sessionStorage.setItem("backgroundColor", "red");
+      sessionStorage.setItem("toastifyContent", response.data.message);
+      setOpenToastify(true);
     }
   };
   return (
@@ -101,7 +96,6 @@ export default function VendorProductList() {
         <div style={{ textAlign: "end" }}>
           <button
             onClick={() => {
-              
               setAddProduct(true);
               setOpenForm(true);
             }}
@@ -111,73 +105,71 @@ export default function VendorProductList() {
           </button>
         </div>
       </div>
-      {
-        loading ? 
-        <Loader/>
-        :
-         productsData.length==0 ?
-        <div style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
-        <h1 style={{marginTop:"1rem"}} >
-          No Product 
-        </h1>
+      {loading ? (
+        <Loader />
+      ) : productsData.length == 0 ? (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <h1 style={{ marginTop: "1rem" }}>No Product</h1>
         </div>
-        :
-      <div className={styles.vendorProducts}>
-        {productsData.map((product:any) => {
-                  // const base64String = window.btoa(
-                  //   String.fromCharCode(...new Uint8Array(product.img.data.data))
-                  // );
-          return(
-          <>
-            <div
-              // onClick={()=>{router.push(`/product-detail/1`)}}
-              className={styles.vendorProductCard}
-            >
-              <RiDeleteBin5Fill
-                onClick={() => deleteProduct(product._id)}
-                className={styles.deleteIcon}
-              />
-              <RiEdit2Fill
-                onClick={() => {
-                  // setProductData({
-                  //   name:"test product edited",
-                  //   productId:"63640fe02434d63b9d344209"
-                  // })
-                  setProductData(product);
-                  setOpenForm(true);
-                  setAddProduct(false);
-                }}
-                className={styles.editIcon}
-              />
-   
-        {/* <img src="https://static.autox.com/uploads/2018/10/Honda-Activa-5G-Image-Gallery-5-.jpg" width="300"/> 
-        */}
-          <Image alt="" src={`data:image/jpeg;base64,${product?.frontImage?.data}`} />
-{/* // how to use image in frontend */}
-{/* // <img src={`data:image/png;base64,${base64String}`} width="300"/> */}
-              {/* <img src="https://static.autox.com/uploads/2018/10/Honda-Activa-5G-Image-Gallery-5-.jpg" /> */}
-              <div
-              onClick={()=>{
-                router.push(`/product-detail/${product?._id}`)
-              }}
-              className={styles.priceSection}>
-                {/* <p>{product.name}</p> */}
-                <p>{product?.name.length<15 ? product?.name :`${product?.name.slice(0,15)}...`}</p>
-                <h3>
-                  <span className={styles.offerPrice}>
-                    ₹{product?.actualPrice}
-                  </span>{" "}
-                  <span className={styles.actualPrice}>
-                    ₹{product?.offerPrice}
-                  </span>
-                </h3>
-              </div>
-            </div>
-          </>
-          )
-})}
-      </div>
-}
+      ) : (
+        <div className={styles.vendorProducts}>
+          {productsData.map((product: any) => {
+            // const base64String = window.btoa(
+            //   String.fromCharCode(...new Uint8Array(product.img.data.data))
+            // );
+            return (
+              <>
+                <div className={styles.vendorProductCard}>
+                  <RiDeleteBin5Fill
+                    onClick={() => deleteProduct(product._id)}
+                    className={styles.deleteIcon}
+                  />
+                  <RiEdit2Fill
+                    onClick={() => {
+                      setProductData(product);
+                      setOpenForm(true);
+                      setAddProduct(false);
+                    }}
+                    className={styles.editIcon}
+                  />
+
+                  <img
+                    alt=""
+                    src={`data:image/jpeg;base64,${product?.frontImage?.data}`}
+                  />
+                  <div
+                    onClick={() => {
+                      router.push(`/product-detail/${product?._id}`);
+                    }}
+                    className={styles.priceSection}
+                  >
+                    {/* <p>{product.name}</p> */}
+                    <p>
+                      {product?.name.length < 15
+                        ? product?.name
+                        : `${product?.name.slice(0, 15)}...`}
+                    </p>
+                    <h3>
+                      <span className={styles.offerPrice}>
+                        ₹{product?.actualPrice}
+                      </span>{" "}
+                      <span className={styles.actualPrice}>
+                        ₹{product?.offerPrice}
+                      </span>
+                    </h3>
+                  </div>
+                </div>
+              </>
+            );
+          })}
+        </div>
+      )}
     </>
   );
 }
